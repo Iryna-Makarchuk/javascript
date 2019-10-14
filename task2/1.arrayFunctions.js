e5ede95780c40192bae731e9f7d8613de7d22ba1
Irene Boboshko <boboshko@cyberiansoft.com>
2019-10-14 19:05:15 +0300

    tasks 2.1-2 added

---
+++ task2/1.arrayFunctions.js
@@ -0,0 +1,57 @@
+/**
+ * Дан масив чисел (положительных, отрицательных и в перемешку)
+ * Найти в массиве: sum, min and max, заменить min and max, используя методы, изученные на занятии.
+ * Создать функцию которая возвращает все эти значения в объекте.
+ * Примеры массивов:
+ * [3,0,-5,1,44,-12,3,0,0,1,2,-3,-3,2,1,4,-2-3-1]
+ * [-1,-8,-2]
+ * [1,7,3]
+ * [1,undefined,3,5,-3]
+ * [1,NaN,3,5,-3]
+ */
+
+const isNumeric = (data) => {
+  return ((data ^ 0) === data) && !Number.isNaN(parseInt(data)) && Number.isFinite(data);
+};
+
+const getNumericArray = (array) => {
+  if (Array.isArray(array)) {
+    let arrayCopy = [];
+    array.forEach((element) => {
+      if (isNumeric(+element)) {
+        arrayCopy.push(+element);
+      }
+    });
+    return arrayCopy;
+  }
+}
+
+const getMinOfArray = (array) => {
+  let numericArray = getNumericArray(array);
+  return numericArray && numericArray.length ? numericArray.sort((prev, next) => prev - next)[0] : null;
+};
+
+const getMaxOfArray = (array) => {
+  let numericArray = getNumericArray(array);
+  return numericArray && numericArray.length ? numericArray.sort((prev, next) => next - prev)[0] : null;
+};
+
+const getSumOfArrayNumbers = (array) => {
+  let numericArray = getNumericArray(array);
+  return numericArray ? numericArray.reduce((acc, num) => acc + num, null) : null;
+};
+
+const getNumericValues = (array) => {
+  return {
+    min: getMinOfArray(array),
+    max: getMaxOfArray(array),
+    sum: getSumOfArrayNumbers(array)
+  }
+}
+
+console.log(getNumericValues([3, 0, -5, 1, 44, -12, 3, 0, 0, 1, 2, '-35', -3, -3, 2, 1, 4, -2 - 3 - 1]));
+console.log(getNumericValues([-1, -8, -2]));
+console.log(getNumericValues([1, 7, '-45', 3]));
+console.log(getNumericValues([1, undefined, 3, 5, -3]));
+console.log(getNumericValues([1, NaN, 3, 5, -3]));
+console.log(getNumericValues([]));
---
+++ task2/2.waterQuantity.js
@@ -0,0 +1,35 @@
+/**
+ * Дан масив чисел, которые представляют собой показатели высоты скал: [2,1,5,0,3,4,7,2,3,1,0]
+ * (для примера дан этот масив, но может быть любой, Ваш алгоритм должен решать все случаи)
+ * Посчитать количество воды (количество синих йчеек), набранной в ямы после дождя.
+ * Нужно по возможности использовать методы массива, а не обычные циклы.
+ * Например, в даном примере правильный ответ: 10
+ * https://docs.google.com/spreadsheets/d/1ew5KhvXAhaB-CxWpYDd_OBYz4yKnaW9k25aCGP0gDG4/edit#gid=0
+ */
+
+let getWaterQuantity = (array) => {
+  let length = array.length, waterCubes = 0, leftPeak = 0, rightPeak = 0, left = 0, right = length - 1;
+
+  while (length--) {
+    array[left] > leftPeak ? leftPeak = array[left]: leftPeak;
+    array[right] > rightPeak ? rightPeak = array[right] : rightPeak;
+
+    if (rightPeak > leftPeak) {
+      waterCubes += leftPeak - array[left];
+      left++;
+    } else {
+      waterCubes += rightPeak - array[right];
+      right--;
+    }
+  }
+
+  return waterCubes;
+}
+
+console.log(getWaterQuantity([2, 5, 1, 3, 1, 2, 1, 7, 7, 6], ' => 17'));
+console.log(getWaterQuantity([2, 1, 5, 0, 3, 4, 7, 2, 3, 1, 0], ' => 10'));
+console.log(getWaterQuantity([7, 0, 1, 3, 4, 1, 2, 1], ' => 9'));
+console.log(getWaterQuantity([2, 1, 5, 0, 3, 4, 7, 2, 3, 1, 0], ' => 10'));
+console.log(getWaterQuantity([2, 2, 1, 2, 2, 3, 0, 1, 2], ' => 4'));
+console.log(getWaterQuantity([2, 1, 5, 0, 3, 4, 7, 2, 3, 1, 8], ' => 24'));
+console.log(getWaterQuantity([2, 2, 2, 2, 2], ' => 0'));
